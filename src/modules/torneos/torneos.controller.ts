828d353c9f33188599a79c7a140470a8d6a0bc20
jhonEdison1 <47945021+jhonEdison1@users.noreply.github.com>
2023-11-21 15:24:34 -0500

    Rol Crear Torneo

--- src/modules/torneos/torneos.controller.ts
+++ src/modules/torneos/torneos.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { TorneosService } from './torneos.service';
 import { CreateTorneoDto } from './dto/create-torneo.dto';
 import { UpdateTorneoDto } from './dto/update-torneo.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { categoria, rama } from '../jugadores/entities/jugadore.entity';
 import { Fases, Modalidad, Tipo } from './entities/torneo.entity';
+import { JwtAuthAccessGuard } from '../iam/guards/jwt-auth.guard';
+import { RolesGuard } from '../iam/guards/roles.guard';
+import { Roles } from '../iam/decorators';
+import { Role } from '../iam/models/roles.model';
 
 
 
@@ -13,6 +17,9 @@ import { Fases, Modalidad, Tipo } from './entities/torneo.entity';
 export class TorneosController {
   constructor(private readonly torneosService: TorneosService) {}
 
+
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthAccessGuard, RolesGuard)
   @Post()
   create(@Body() createTorneoDto: CreateTorneoDto) {
     return this.torneosService.create(createTorneoDto);
