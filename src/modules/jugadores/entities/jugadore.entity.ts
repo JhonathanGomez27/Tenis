04668b24917b183b0a412358fb8103a3f66e075d
Miguel Alzate <alzatemiguel284@gmail.com>
2024-09-17 11:52:56 -0500

    Changed categorias to nullables

--- src/modules/jugadores/entities/jugadore.entity.ts
+++ src/modules/jugadores/entities/jugadore.entity.ts
@@ -1,59 +1,60 @@
-import { Usuario } from "src/modules/usuarios/entities/usuario.entity";
-import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
-
+import { Usuario } from 'src/modules/usuarios/entities/usuario.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 export enum rama {
-    MASCULINA = 'masculina',
-    FEMENINA = 'femenina',
-    MIXTA = 'mixta'
+  MASCULINA = 'masculina',
+  FEMENINA = 'femenina',
+  MIXTA = 'mixta',
 }
-
 
 export enum categoria {
-    A = 'A',
-    BMAS = 'B+',
-    B = 'B',
-    CMAS = 'C+',
-    C = 'C',
-    D = 'D',
+  A = 'A',
+  BMAS = 'B+',
+  B = 'B',
+  CMAS = 'C+',
+  C = 'C',
+  D = 'D',
 }
-
-
 
 @Entity({ name: 'jugadores' })
 export class Jugador {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Column()
+  nombre: string;
 
-    @Column()
-    nombre: string;
+  @Column({ default: 0 })
+  ranking: number;
 
-    @Column({ default: 0 })
-    ranking: number;
+  @Column({
+    type: 'enum',
+    enum: rama,
+  })
+  rama: 'masculina' | 'femenina';
 
-    @Column({
-        type: 'enum',
-        enum: rama
-    })
-    rama: 'masculina' | 'femenina'
+  @Column({
+    type: 'enum',
+    enum: categoria,
+    nullable: true,
+  })
+  categoria: 'A' | 'B+' | 'B' | 'C+' | 'C' | 'D';
 
-    @Column({
-        type: 'enum',
-        enum: categoria,
-        default: categoria.D
-    })
-    categoria: 'A' | 'B+' | 'B' | 'C+' | 'C' | 'D'
+  @Column({
+    type: 'enum',
+    enum: categoria,
+    nullable: true,
+  })
+  categoria_dobles: 'A' | 'B+' | 'B' | 'C+' | 'C' | 'D';
 
-    @Column({
-        type: 'enum',
-        enum: categoria,
-        default: categoria.D
-    })
-    categoria_dobles: 'A' | 'B+' | 'B' | 'C+' | 'C' | 'D'
-
-
-    @OneToOne(() => Usuario)
-    @JoinColumn({ name: 'useridId' })
-    userid: Usuario;
+  @OneToOne(() => Usuario)
+  @JoinColumn({ name: 'useridId' })
+  userid: Usuario;
 }
